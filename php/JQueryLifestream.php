2701c25f6dc914a9ec6f563af38774dbcb5bd58b
mgiulio <giulio.mainardi@gmail.com>
2013-04-12 15:36:43 +0200

    First working implementation with shortcode

--- php/JQueryLifestream.php
+++ php/JQueryLifestream.php
@@ -6,6 +6,30 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 
 	function __construct() {
 		parent::__construct(array());
+		
+		add_shortcode('jls', array($this, 'run_shortcode'));
+	}
+	
+	function run_shortcode() {
+		$jsl_handle = $this->plugin_prefix . 'jls_js';
+		wp_enqueue_script(
+			$jsl_handle,
+			"{$this->url['js']}jls/jquery.lifestream.min.js",
+			array('jquery'), 
+			'', 
+			true
+		);
+		wp_enqueue_script(
+			$this->plugin_prefix . 'run_jls_js',
+			"{$this->url['js']}run_jls.js",
+			array($jsl_handle), 
+			'', 
+			true
+		);
+		
+		$html = '<div class="jls_container"></div>';
+		
+		return $html;
 	}
 	
 }

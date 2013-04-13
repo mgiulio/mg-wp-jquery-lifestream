446193ceb909e73f2c9af0c8cda9c0290c956d16
mgiulio <giulio.mainardi@gmail.com>
2013-04-13 15:20:16 +0200

    Settings: services rendering

--- php/JQueryLifestream.php
+++ php/JQueryLifestream.php
@@ -7,6 +7,8 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 	function __construct() {
 		parent::__construct(array());
 		
+		$this->install_option();
+		
 		$this->menu_slug = 'jls';
 		$this->settings_group = $this->menu_slug;
 		
@@ -14,6 +16,29 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 		add_action('admin_menu', array($this, 'setup_menu'));
 		
 		add_shortcode('jls', array($this, 'run_shortcode'));
+	}
+	
+	private function install_option() {
+		if (get_option('jls'))
+			return;
+		
+		update_option('jls', array(
+			'services' => array(
+				'bitbucket' => array('user' => ''),
+				'bitly' => array('user' => ''),
+				'blogger' => array('user' => ''),
+				'citeulike' => array('user' => ''),
+				'dailymotion' => array('user' => ''),
+				'delicious' => array('user' => ''),
+				'deviantart' => array('user' => ''),
+				'disqus' => array('user' => ''),
+				'dribble' => array('user' => ''),
+				'facebook_page' => array('user' => ''),
+				'flickr' => array('user' => ''),
+				'foomark' => array('user' => ''),
+				'formspring' => array('user' => ''),
+			)
+		));
 	}
 	
 	function setup_menu() {
@@ -27,6 +52,12 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 	}
 	
 	function setup_settings() {
+		register_setting(
+			$this->settings_group, 
+			'jls'//,
+			//array($this, $validation)
+		);
+		
 		add_settings_section(
 			'jls_services', 
 			'Services', 
@@ -34,36 +65,22 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 			$this->menu_slug
 		);
 		
-		add_settings_field(
-			"jls_services_twitter_user",
-			'Twitter',
-			array($this, 'render_twitter'),
-			$this->menu_slug,
-			'jls_services'
-		);
-
-		register_setting(
-			$this->settings_group, 
-			'jls'//,
-			//array($this, $validation)
-		);
+		$cfg = get_option('jls');
+		$services = $cfg['services'];
+		foreach ($services as $service_name => $service_cfg) {
+			$service_renderer = new ServiceRenderer($service_name, $service_cfg);
+			add_settings_field(
+				"jls_services_{$service_name}_user",
+				$service_name,
+				array($service_renderer, 'render'),
+				$this->menu_slug,
+				'jls_services'
+			);
+		}
 	}
 	
 	function services_desc() {
 		echo "Fill in the username for the services you want to include in the lifestream";
-	}
-	
-	function render_twitter() {
-		$cfg = get_option('jls');
-		$service_cfg = $cfg['services']['twitter'];
-		$user = $service_cfg['user'];
-		?>
-			<input 
-				name="jls[services][twitter][user]" 
-				type="text" 
-				value="<?php echo $user ?>"
-			>
-		<?php
 	}
 	
 	function render_menu_page() {
@@ -106,6 +123,25 @@ final class mgJQueryLifestream extends mgJQueryLifestreamBase  {
 		$html = '<div class="jls_container"></div>';
 		
 		return $html;
+	}
+	
+}
+
+class ServiceRenderer {
+	
+	function __construct($name, $cfg) {
+		$this->name = $name;
+		$this->cfg = $cfg;
+	}
+	
+	function render() {
+		?>
+			<input 
+				name="jls[services][<?php echo $this->name; ?>][user]" 
+				type="text" 
+				value="<?php echo $this->cfg['user']; ?>"
+			>
+		<?php
 	}
 	
 }

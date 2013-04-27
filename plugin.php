b61b68dd99e0729e98628be45513b547085ccea6
mgiulio <giulio.mainardi@gmail.com>
2013-04-27 08:33:18 +0200

    Update plugin header meta

--- plugin.php
+++ plugin.php
@@ -2,9 +2,9 @@
 
 /*
 Plugin Name: mg WP jQuery Lifestream
-Plugin URI: http://mgiulio.altervista.org
-Description: blah blah ....
-Version: 0.1
+Plugin URI: http://mgiulio.altervista.org/projects/wordpress/plugins/mg-wp-jquery-lifestream
+Description: Include a social lifestream with jQuery Lifestream.
+Version: 1.0 beta
 Author: mgiulio (Giulio Mainardi)
 Author URI: http://mgiulio.altervista.org
 License: GPLv2
